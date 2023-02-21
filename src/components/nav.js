6b45872568da30f1481f741fc242aeac426db223
cosmic <yafetyohaness@gmail.com>
2023-02-22 01:59:37 +0300

    add logout

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,6 +1,19 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const NavBar = ({ user }) => {
+  const navigate = useNavigate();
+  const logout = () => {
+    fetch("http://localhost:5000/logout", {
+      method: "POST",
+      mode: "cors",
+      credentials: "include",
+    }).then((res) => {
+      if (res.status === 200) {
+        navigate("/");
+      }
+    });
+  };
   return (
     <div className="m-auto py-3 text-white bg-slate-700">
       <div className=" max-w-screen-lg m-auto flex justify-around items-center h-16">
@@ -9,7 +22,10 @@ export const NavBar = ({ user }) => {
         </Link>
 
         {user && (
-          <button className=" uppercase shadow bg-red-800 hover:bg-red-700 focus:shadow-outline focus:outline-none text-white  py-3 px-4 rounded">
+          <button
+            onClick={logout}
+            className=" uppercase shadow bg-red-800 hover:bg-red-700 focus:shadow-outline focus:outline-none text-white  py-3 px-4 rounded"
+          >
             logout
           </button>
         )}
